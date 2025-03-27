a79a5ae6e47132234e3b15d47e72bc8eda8b665c
Mykola_Tarasenko <mykola.tarasenko@campus.tu-berlin.de>
2025-03-27 23:29:42 +0200

    context 1

--- main/context_processors.py
+++ main/context_processors.py
@@ -5,45 +5,71 @@ from .models import FooterInfo, Product
 
 
 def footer_info(request):
-    data = FooterInfo.objects.first()
+    if FooterInfo.objects.exists():
 
-    return {
-        'phone' : data.phone,
-        'email' : data.email,
-        'address' : data.address,
-        'like_info' : data.like_info,
+        data = FooterInfo.objects.first()
 
-        # links
-        'like_info_more' : data.like_info_more,
-        'about_us' : data.about_us,
-        'contact_us': data.contact_us,
-        'privacy_policy' : data.privacy_policy,
-        'terms_and_conditions' : data.terms_and_conditions,
-        'return_policy': data.return_policy,
-        'faqs_and_help': data.faqs_and_help,
+        return {
+            'phone' : data.phone,
+            'email' : data.email,
+            'address' : data.address,
+            'like_info' : data.like_info,
 
-        # account
-        'account': data.account,
-        'shop_details' : data.shop_details,
-        'shopping_cart' : data.shopping_cart,
-        'order_history' : data.order_history,
-    }
+            # links
+            'like_info_more' : data.like_info_more,
+            'about_us' : data.about_us,
+            'contact_us': data.contact_us,
+            'privacy_policy' : data.privacy_policy,
+            'terms_and_conditions' : data.terms_and_conditions,
+            'return_policy': data.return_policy,
+            'faqs_and_help': data.faqs_and_help,
+
+            # account
+            'account': data.account,
+            'shop_details' : data.shop_details,
+            'shopping_cart' : data.shopping_cart,
+            'order_history' : data.order_history,
+        }
+    else:
+        return {
+            'phone': '',
+            'email': '',
+            'address': '',
+            'like_info': '',
+
+            # links
+            'like_info_more': '',
+            'about_us': '',
+            'contact_us': '',
+            'privacy_policy': '',
+            'terms_and_conditions': '',
+            'return_policy': '',
+            'faqs_and_help': '',
+
+            # account
+            'account': '',
+            'shop_details': '',
+            'shopping_cart': '',
+            'order_history': '',
+        }
 
 def count_product_ratings(request):
-    products = Product.objects.filter(is_visible=True).order_by('sort')
-    for product in products:
-        testimonials = product.testimonials.filter(is_appropriate=True)
-        if testimonials.exists():
-            rate = sum(testimonial.rate for testimonial in testimonials) / len(testimonials)
-            product.rating = rate
-            product.is_bestseller=(rate>=4)
-        else:
-            product.rating = 5
-            product.is_bestseller=True
-        product.save()
-    bestseller_products = Product.objects.filter(is_visible=True, is_bestseller=True).order_by('sort')
-    # Debugging: Print bestseller products in the console
+    if Product.objects.exists():
+
+        products = Product.objects.filter(is_visible=True).order_by('sort')
+        for product in products:
+            testimonials = product.testimonials.filter(is_appropriate=True)
+            if testimonials.exists():
+                rate = sum(testimonial.rate for testimonial in testimonials) / len(testimonials)
+                product.rating = rate
+                product.is_bestseller=(rate>=4)
+            else:
+                product.rating = 5
+                product.is_bestseller=True
+            product.save()
+        bestseller_products = Product.objects.filter(is_visible=True, is_bestseller=True).order_by('sort')
+        # Debugging: Print bestseller products in the console
 
 
-    return {'products' : products, 'bestseller_products': bestseller_products}
-
+        return {'products' : products, 'bestseller_products': bestseller_products}
+    return {'products' : '', 'bestseller_products': ''}
